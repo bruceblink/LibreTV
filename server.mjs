32f4a49cf2373ae6f88f6ec968c060451e8c234e
likanug <likanug.g@qq.com>
2025-06-26 12:03:57 +0800

    fix: 移除未使用的请求参数，简化中间件函数

--- server.mjs
+++ server.mjs
@@ -38,7 +38,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use((req, res, next) => {
+app.use((_, res, next) => {
   res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('X-Frame-Options', 'SAMEORIGIN');
   res.setHeader('X-XSS-Protection', '1; mode=block');
